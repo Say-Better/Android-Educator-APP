80f2187e5c000bbf35892ca46c07a0f3c87fe03f
yeongin-ji <jyi0106@gachon.ac.kr>
2024-07-04 22:21:50 +0900

    ✨ Feat: Color 정의

--- app/src/main/java/com/example/saybettereducator/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/saybettereducator/ui/login/LoginActivity.kt
@@ -1,4 +1,41 @@
 package com.example.saybettereducator.ui.login
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+class LoginActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            LoginScreen()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun LoginPreview() {
+        LoginScreen()
+    }
+
+    @Composable
+    fun LoginScreen() {
+        Surface {
+            Column {
+                Text(text = "만나서 반가워요!\nSay Better를 시작해볼까요?")
+
+                Image(painter = painterResource(id = R.drawable.), contentDescription = )
+            }
+        }
+    }
 }
+
