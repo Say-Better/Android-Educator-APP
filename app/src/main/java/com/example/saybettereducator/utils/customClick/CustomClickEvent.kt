d81accb43be79daa6f19f5978219d8e42bc6e090
yeongin-ji <jyi0106@gachon.ac.kr>
2024-07-07 15:33:47 +0900

    ✨ Feat: CustomClickEvent 추가
    #4

--- app/src/main/java/com/example/saybettereducator/utils/customClick/CustomClickEvent.kt
+++ app/src/main/java/com/example/saybettereducator/utils/customClick/CustomClickEvent.kt
@@ -1,4 +1,30 @@
 package com.example.saybettereducator.utils.customClick
 
-object CustomClickEvent {
+import android.util.Log
+import androidx.compose.foundation.IndicationNodeFactory
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.node.DelegatableNode
+import androidx.compose.ui.node.DrawModifierNode
+
+object CustomClickEvent : IndicationNodeFactory {
+    private class DefaultDebugIndicationNode(
+        private val interactionSource: InteractionSource,
+    ) : Modifier.Node(), DrawModifierNode {
+        override fun ContentDrawScope.draw() {
+            Log.d("indication", "contentDrawScope")
+            drawRect(color = Color.Gray.copy(alpha = 0f), size = size)
+            this@draw.drawContent()
+        }
+    }
+
+    override fun create(interactionSource: InteractionSource): DelegatableNode {
+        return DefaultDebugIndicationNode(interactionSource)
+    }
+
+    override fun equals(other: Any?): Boolean = other === this
+
+    override fun hashCode(): Int = -1
 }
