f58fcf394e98eef14658601e975046a8bd581429
yeongin-ji <jyi0106@gachon.ac.kr>
2024-07-10 12:08:52 +0900

    ✨ Feat: MyEventListener, AppModule 추가
    #6: AppModule - Hilt 의존성 주입 관련

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
