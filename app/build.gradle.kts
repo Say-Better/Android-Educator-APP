d1f5b45a057763581da1aacea69c4730b521f1e1
luke0408 <zjvl324@gmail.com>
2024-07-28 23:54:16 +0900

    :wrench: Chore: test 의존성 추가
    <footer>
    - 관련: #10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
