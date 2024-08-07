f5109dc6e533b76ce2bd7e65595553e01add6acb
luke0408 <zjvl324@gmail.com>
2024-08-08 00:06:28 +0900

    :sparkles: Feat: UserInfoComponents.kt 분리
    <footer>
    - 관련: #13

---
+++ app/src/main/java/com/example/saybettereducator/ui/view/components/UserInfoBottomBar.kt
@@ -0,0 +1,49 @@
+package com.example.saybettereducator.ui.view.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.saybettereducator.ui.theme.MainGreen
+import com.example.saybettereducator.ui.theme.PretendardTypography
+import com.example.saybettereducator.ui.theme.White
+
+@Composable
+fun UserInfoBottomBar(
+    text: String,
+    onBottomBarClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(88.dp)
+    ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(
+                color = MainGreen, shape = RoundedCornerShape(size = 32.dp)
+            )
+            .clickable (onClick = onBottomBarClick)
+        ) {
+            Text(
+                text = text,
+                style = PretendardTypography.buttonLarge.copy(White),
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/saybettereducator/ui/view/components/UserInfoTopAppBar.kt
@@ -0,0 +1,44 @@
+package com.example.saybettereducator.ui.view.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.saybettereducator.R
+import com.example.saybettereducator.ui.theme.MainGreen
+import com.example.saybettereducator.ui.theme.montserratFont
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun UserInfoTopAppBar() {
+    Box {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Say Better",
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(montserratFont),
+                    color = MainGreen,
+                    modifier = Modifier.padding(start = 34.04.dp, top = 15.29.dp)
+                )
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.main_logo),
+            contentDescription = "main top bar logo",
+            modifier = Modifier.padding(start = 16.dp, top = 14.29.dp)
+        )
+    }
+}
